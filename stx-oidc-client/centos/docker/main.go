b1c018a79a90b76f6ce261ae165fd76562ebb861
Teresa Ho <teresa.ho@windriver.com>
2020-02-13 16:04:00 -0500

    Refresh token missing in the response from dex
    The refresh token is not returned from dex because the offline_access
    scope option was not passed to dex. This update is to ensure that the
    offline_access is set.
    
    Closes-Bug: 1863264
    
    Change-Id: I93a37fde66414d5557dc96fb00f002621005e80b
    Signed-off-by: Teresa Ho <teresa.ho@windriver.com>

--- stx-oidc-client/centos/docker/main.go
+++ stx-oidc-client/centos/docker/main.go
@@ -276,9 +276,7 @@ func (a *app) handleLogin(w http.ResponseWriter, r *http.Request) {
 
     authCodeURL := ""
     scopes = append(scopes, "openid", "profile", "email")
-    if r.FormValue("offline_access") != "yes" {
-        authCodeURL = a.oauth2Config(scopes).AuthCodeURL(exampleAppState)
-    } else if a.offlineAsScope {
+    if a.offlineAsScope {
         scopes = append(scopes, "offline_access")
         authCodeURL = a.oauth2Config(scopes).AuthCodeURL(exampleAppState)
     } else {
